ee81ce39197394af8b25d0c9e8a487ffa5b80b0d
Jsgeeker <banny560008@vip.qq.com>
2015-02-05 21:21:09 +0800

    delete start.sh, use gulp to start or restart

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,27 @@ var gulpSequence = require('gulp-sequence');
 var clean        = require('gulp-clean');
 var copy         = require('gulp-copy');
 var less         = require('gulp-less');
-var watch        = require('gulp-watch');
 var react        = require('gulp-react');
+var nodemon      = require("gulp-nodemon");
 
 var path         = require('path');
 
 var version      = 'v' + require('./package.json').version;
+// Single instance
+var nodemon_instance;
 
+// Sub tasks
 gulp.task('clean', function () {
   return gulp.src(['static/dist/*'])
     .pipe(clean({force: true}));
 });
 
 gulp.task('copy', function () {
-  return gulp.src(['static/src/**/*'])
+  return gulp.src(['static/src/images/**/*'])
     .pipe(copy('static/dist/', {prefix: 2}));
 });
 
-gulp.task('less', function () {
+gulp.task('less-compile', function () {
   return gulp.src('static/src/less/**/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
@@ -30,17 +33,32 @@ gulp.task('less', function () {
     .pipe(gulp.dest('static/dist/css'));
 });
 
-gulp.task('react', function () {
+gulp.task('jsx-compile', function () {
     return gulp.src('static/src/jsx/**/*.jsx')
         .pipe(react())
         .pipe(gulp.dest('static/dist/js'));
 });
 
 gulp.task('watch', function () {
-    return watch('static/src/*')
-        .pipe(gulp.dest('static/dist/'));
+  gulp.watch('static/src/jsx/**/*.jsx', ['jsx-compile']);
+  gulp.watch('static/src/less/**/*.less', ['less-compile']);
 });
 
-gulp.task('default', gulpSequence('clean', 'copy', ['less', 'react']));
-gulp.task('dev', ['default', 'watch']);
-gulp.task('build', ['default']);
+gulp.task('nodemon', function () {
+  if(!nodemon_instance){ // Start server
+    nodemon_instance = nodemon({ 
+      script:"app.js", 
+      nodeArgs: ["--harmony", "--debug"],
+      env: { "NODE_ENV": "dev" }, 
+      watch: "__manual_watch__",  
+      ext: "__manual_watch__"  
+    });
+  }else { // Restart server
+    nodemon_instance.emit("restart");
+  }
+});
+
+// Main stask
+gulp.task('install', gulpSequence('clean', 'copy', ['less-compile', 'jsx-compile']));
+gulp.task('dev', gulpSequence('install', ['watch'], 'nodemon'));
+gulp.task('build', gulpSequence('install', 'nodemon'));
